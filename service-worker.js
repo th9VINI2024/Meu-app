6e873d55d5645e41846b663f4d38ac549ae119e6
th9VINI2024 <161654715+th9VINI2024@users.noreply.github.com>
2026-02-06 11:56:56 -0300

    Implement service worker for caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,18 @@
 
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open("player-cache").then(c=>{
+      return c.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
